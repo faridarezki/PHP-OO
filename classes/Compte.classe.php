84eca2af13a157095d052d7bf2fb3142a0020cfa
arezki-max <faridd.arezki@yahoo.fr>
2021-10-10 17:18:49 +0200

    create file Note.txt

--- classes/Compte.classe.php
+++ classes/Compte.classe.php
@@ -18,9 +18,27 @@ public $titulaire;
  * @var float
  */
 
-public $solde="100";
+public $solde;
+
+public function __construct(string $titulaire,float $solde)
+{
+$this->titulaire=$titulaire;
+$this->solde=$solde;
+}
+public function verif(){
+
+if($this->solde<0){
+  echo "Attention vous êtes à decouvert de ".abs($this->solde)." euros";
+}else {
+    echo "vous avez un solde de ".$this->solde;
+}
 
 }
+    
+}
+
+
+
 
 
 
--- index.php
+++ index.php
@@ -15,13 +15,16 @@
      * On instancie le compte (creation d'un objet)
      */
 
-    $compte1 = new Commpte();
+    $compte1 = new Commpte("Arezki", -100);
     //On ecrit dans la propriété titulaire
-    $compte1->titulaire = "Arezki";
-    $compte2 = new Commpte();
-    var_dump($compte1)
 
+    $compte2 = new Commpte("Ouhenia", 200);
 
+    var_dump($compte1);
+    echo '<br>';
+    var_dump($compte2);
+    echo '<br>';
+$compte1->verif();
     ?>
 </body>
 
