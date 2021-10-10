044f69dd8e6141f03b7c9ea70ba5d48f23edc9cf
arezki-max <faridd.arezki@yahoo.fr>
2021-10-10 19:34:36 +0200

    create file style.css

--- index.php
+++ index.php
@@ -2,27 +2,28 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>POO PHP</title>
 </head>
 
 <body>
-    <?php
-    require_once 'classes/Compte.classe.php';
-    /**
-     * On instancie le compte (creation d'un objet)
-     */
+  <?php
+  require_once 'classes/Compte.classe.php';
+  /**
+   * On instancie le compte (creation d'un objet)
+   */
 
-    $compte1 = new Commpte("Arezki", 1000);
-    //On ecrit dans la propriété titulaire
-  $res=  $compte1->retirer(990);
+  $compte1 = new Commpte("Arezki", 1000);
+  //On ecrit dans la propriété titulaire
+  $res =  $compte1->retirer(990);
   echo $res;
-    echo '<br>';
- $compte1->consulterSolde();
+  echo '<br>';
+  $compte1->consulterSolde();
 
-    ?>
+  ?>
 </body>
 
 </html>
